bb1fc03ed31422c52bb953602f1999256c391cd3
Mitsuki.Fukunaga <Mitsuki.Fukunaga@tasplan.com.au>
2021-07-21 10:34:26 +1000

    v3 migration

--- background.js
+++ background.js
@@ -1,12 +1,13 @@
 (function() {
-  chrome.browserAction.onClicked.addListener(function (tab) {
+  chrome.action.onClicked.addListener(tab => {
     chrome.tabs.sendMessage(tab.id, {text: 'download_transcript'});
   });
-  chrome.commands.onCommand.addListener(function(command) {
-    if(command === "download_transcript"){
-      chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {text: 'download_transcript'});
-      });
-    }
-  });
 })();
+
+chrome.commands.onCommand.addListener(async (command) => {
+  if (command === "download_transcript") {
+    chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+      chrome.tabs.sendMessage(tabs[0].id, {text: 'download_transcript'});
+    });
+  }
+});

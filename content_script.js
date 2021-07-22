2f447f78f5fe6d759d597df2a4da88787605ca47
Mitsuki Fukunaga <m@Mitsukis-iMac.local>
2021-07-23 09:44:13 +1000

    refactor: removed jQuery

--- content_script.js
+++ content_script.js
@@ -1,9 +1,9 @@
-chrome.runtime.onMessage.addListener( (msg, sender, sendResponse) => {
-  if (msg.text === 'download_transcript' && $('.conversation__view__stream-wrapper').length) {
-    const dom = $('.conversation__view__stream-wrapper').clone();
-    const conversation_id = dom.data('conversation-id');
-    dom.find('.conversation__card__content-expanded__controls').remove().end()
-    const sspage = new XMLSerializer().serializeToString(dom[0]);
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+  if (msg.text === 'download_transcript' && document.querySelector('.conversation__view__stream-wrapper')) {
+    const dom = document.querySelector('.conversation__view__stream-wrapper').cloneNode(true);
+    const conversation_id = dom.dataset.conversationId;
+    dom.querySelector('.conversation__card__content-expanded__controls').remove();
+    const sspage = new XMLSerializer().serializeToString(dom);
     const blob = new Blob(['<!DOCTYPE html><html lang="en"><head><link rel="stylesheet" href="https://mtskf.github.io/intercom.css"></head><body>' + sspage + '</body></html>'], { "type" : "text/plain" });
     const url = window.URL;
     const bloburl = url.createObjectURL(blob);
@@ -12,5 +12,6 @@ chrome.runtime.onMessage.addListener( (msg, sender, sendResponse) => {
     a.href = bloburl;
     a.click();
     a.remove();
+    dom.remove();
   }
 });
